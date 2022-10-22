344206a5ea9b9ae3e4a6145a98ee395dfeddcf9f
BhaviniMadhuranath <bhaviniphillippe@gmail.com>
2022-10-22 06:12:01 +0530

    added ping slash command

--- cogs/server.py
+++ cogs/server.py
@@ -3,6 +3,7 @@ from discord.ext import commands, tasks
 import os
 import asyncio
 import base64
+from discord_slash import cog_ext, utils
 from discord.utils import get
 from datetime import datetime
 from selenium import webdriver
@@ -23,15 +24,11 @@ VERIFIED = 1032714872695304203
 SENIOR = 1032714925832949820
 
 
-
-
-
-
 class server(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        self.uptimeinfo ="`p!uptime` or `p!ut`\n\n\nShows how long the bot has been online for\n\n"
+        self.uptimeinfo = "`p!uptime` or `p!ut`\n\n\nShows how long the bot has been online for\n\n"
         self.loadit = "`p!loadit <extention>`\n\n\nLoads the extention\n\n"
         self.unloadit = "`p!unloadit <extention>`\n\nUnloads the extention\n\n"
         self.polsho = "`p!pollshow` or `p!ps <pollMsgLink>`\n\nShows the results of the poll\n\n"
@@ -40,7 +37,7 @@ class server(commands.Cog):
         self.rest = "`p!restart`\n\nRestarts the bot :flushed:\n\n"
         self.confessen = "`p!enableconfess`\n\n\nEnables the confess feature\n\n"
         self.confessdis = "`p!disableconfess`\n\n\nDisables the confess feature\n\n"
-	#self.confbanusr =
+        # self.confbanusr =
         self.veri = '`p!v` or `p!verify`\np!v help\np!v {SRN}\n\n'
         self.count = '`p!c` or `p!count <role [Not mention]>`\n\nReturns the number of people with the specified role\n\n'
         self.ping = '`p!ping` or `p!Ping`\n\n\nReturns the bot\'s latency\n\n'
@@ -57,10 +54,9 @@ class server(commands.Cog):
         self.unlock = '`p!unlock <channel>`\n\n\nUnlocks the specified channel\n\n'
         self.kick = '`p!kick <Mention> <Reason>`\n\nKicks the member from the server\n\n'
         self.snipeinfo = "`p!snipe`\n\n\nResends the last deleted message on the server\n\n"
-	# self.checkPESUAnnouncement.start()
+        # self.checkPESUAnnouncement.start()
         # self.checkNewDay.start()
         self.snipe = None
-
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -79,14 +75,12 @@ class server(commands.Cog):
             activity=discord.Game(name="with the PRIDE of PESU"),
         )
 
-
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         # error handling, in case of an error the error message will be put up in the channel
         string = f"Something's wrong, I can feel it\n{str(error)}"
         await ctx.channel.send("``{}``".format(string))
         await self.client.get_channel(BOT_LOGS).send(f"{string}\n{str(ctx.message.author.mention)} is a noob who made this mistake in {str(ctx.message.channel.mention)}")
-
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -100,12 +94,10 @@ class server(commands.Cog):
         if(helpers(self.client).getDeverified(str(user.id))):
             await self.client.get_channel(BOT_LOGS).send("Deverified the user")
 
-
-    #@commands.Cog.listener()
-    #async def on_member_update(self, before, after):
+    # @commands.Cog.listener()
+    # async def on_member_update(self, before, after):
     #    if((self.budday not in before.roles) and (self.budday in after.roles)):
     #        await self.client.get_channel(798472825589334036).send(f"Yo, it's {before.mention}'s birthday!")
-
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -113,25 +105,30 @@ class server(commands.Cog):
             pass
         else:
             temp = message.content.replace("`", "|")
-            if (f'<@!{BOT_DEVS}>' in str(temp)): # Bot devs
+            if (f'<@!{BOT_DEVS}>' in str(temp)):  # Bot devs
                 ping_log = f"{message.author.mention} pinged botdev in {message.channel.mention}"
                 ping_embed = discord.Embed(title="Ping", color=0x0000ff)
-                ping_embed.add_field(name="Ping report", value=ping_log, inline=False)
-                ping_embed.add_field(name="Message content", value=f"https://discord.com/channels/{GUILD_ID}/{message.channel.id}/{message.id}", inline=False)
+                ping_embed.add_field(name="Ping report",
+                                     value=ping_log, inline=False)
+                ping_embed.add_field(
+                    name="Message content", value=f"https://discord.com/channels/{GUILD_ID}/{message.channel.id}/{message.id}", inline=False)
                 await self.client.get_channel(MOD_LOGS).send(embed=ping_embed)
-            if (f'<@&{MODS}>' in str(temp)) : #Time keepers
+            if (f'<@&{MODS}>' in str(temp)):  # Time keepers
                 ping_log = f"{message.author.mention} pinged mods in {message.channel.mention}"
                 ping_embed = discord.Embed(title="Ping", color=0x0000ff)
-                ping_embed.add_field(name="Ping report", value=ping_log, inline=False)
-                ping_embed.add_field(name="Message content", value=f"https://discord.com/channels/{GUILD_ID}/{message.channel.id}/{message.id}", inline=False)
+                ping_embed.add_field(name="Ping report",
+                                     value=ping_log, inline=False)
+                ping_embed.add_field(
+                    name="Message content", value=f"https://discord.com/channels/{GUILD_ID}/{message.channel.id}/{message.id}", inline=False)
                 await self.client.get_channel(MOD_LOGS).send(embed=ping_embed)
             if (f'<@&{ADMIN}>' in str(temp)):
                 ping_log = f"{message.author.mention} pinged admin in {message.channel.mention}"
                 ping_embed = discord.Embed(title="Ping", color=0x0000ff)
-                ping_embed.add_field(name="Ping report", value=ping_log, inline=False)
-                ping_embed.add_field(name="Message content", value=f"https://discord.com/channels/{GUILD_ID}/{message.channel.id}/{message.id}", inline=False)
+                ping_embed.add_field(name="Ping report",
+                                     value=ping_log, inline=False)
+                ping_embed.add_field(
+                    name="Message content", value=f"https://discord.com/channels/{GUILD_ID}/{message.channel.id}/{message.id}", inline=False)
                 await self.client.get_channel(MOD_LOGS).send(embed=ping_embed)
-
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -148,7 +145,6 @@ class server(commands.Cog):
                                 await rr.remove(user)
             except:
                 pass
-
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
@@ -174,7 +170,6 @@ class server(commands.Cog):
                 pass
             self.snipe = None
 
-
     @commands.command(aliases=['h', 'help'])
     async def _help(self, ctx):
         help_embed = discord.Embed(title="PESU BOT", color=0x48BF91)
@@ -194,8 +189,10 @@ class server(commands.Cog):
             #help_embed.add_field(name="Purge", value=self.purge)
             help_embed.add_field(name="Echo", value=self.echo)
             if((self.admin in ctx.author.roles) or (self.mods in ctx.author.roles)):
-                help_embed.add_field(name="Enable Confess", value=self.confessen)
-                help_embed.add_field(name="Disable Confess", value=self.confessdis)
+                help_embed.add_field(name="Enable Confess",
+                                     value=self.confessen)
+                help_embed.add_field(
+                    name="Disable Confess", value=self.confessdis)
                 help_embed.add_field(name="Mute", value=self.mute)
                 help_embed.add_field(name="Unmute", value=self.unmute)
                 help_embed.add_field(name="Lock", value=self.lock)
@@ -210,12 +207,14 @@ class server(commands.Cog):
                 help_embed.add_field(name="Scrape", value=self.scrape)
         await ctx.channel.send(embed=help_embed)
 
-
-    @commands.command(aliases=['ping', 'Ping'])
+    # @commands.command(aliases=['ping', 'Ping'])
+    @cog_ext.cog_slash(name="ping",
+                       description="displays the ping",
+                       options=[[]]
+                       )
     async def _ping(self, ctx):
         ps = f"Pong!!!\nPing = `{str(round(self.client.latency * 1000))} ms`"
         await ctx.channel.send(ps)
-
 
     def getDeverified(self, a=""):
         dat = ""
